91f0f67dae447bd5ef22f8748cb0f9d201ec85ff
aharon weber <a.w770770@gmail.com>
2026-06-10 21:42:01 +0300

    Added main file and added fastapi directory

---
+++ app/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from routers.book.routes import router as router_book
+from routers.member.routes import router as router_member
+from routers.report.routes import router as router_report
+
+
+
