4737c45528e641500fa42880fd2f20c8d0da3f6c
aharon weber <a.w770770@gmail.com>
2026-06-10 22:08:52 +0300

    Adding README and testing server

--- app/main.py
+++ app/main.py
@@ -3,5 +3,11 @@ from routers.book.routes import router as router_book
 from routers.member.routes import router as router_member
 from routers.report.routes import router as router_report
 
+app = FastAPI()
+
+app.include_router(router_book)
+app.include_router(router_member)
+app.include_router(router_report)
+
 
 
--- app/routers/book/routes.py
+++ app/routers/book/routes.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter
 
 router = APIRouter() 
+
+
+@router.get("/")
+def get_root():
+    return{"message": "server rannung"}
+
